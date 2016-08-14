13b43124f4722c289ac7a954b90ebbed0d3ffff1
hkmshb <hkmshb@gmail.com>
2016-08-14 13:20:10 +0100

    Added classes: SNGen, HallowIndicator & EPXEngine.

--- epx/core.py
+++ epx/core.py
@@ -1,7 +1,13 @@
 """
 Defines the core objects for ePinXtractr.
 """
+import shutil
+import os.path
+import xml.etree.ElementTree as ET
+from datetime import datetime
 from collections import namedtuple
+
+from dolfin import Storage as _ 
 
 
 
@@ -81,3 +87,176 @@ class Packt(namedtuple('Packt', 'pins, value, quantity')):
                     raise Value("Record field value is invalid: %s" % parts[0])
         
         return Packt(*args)
+
+
+class SNGen(object):
+    """Represents a serial number generator of some sort which embeds a time
+    stamp at the start of generated number sequences. The numbers that follow
+    the timestamp are sequence and begin at indicated offset.
+    """
+
+    MIN_SERIAL_LENGTH = 14
+    TIMESTAMP_LENGTH  = 10
+
+    def __init__(self, timestamp=datetime.now(), offset=1, length=20):
+        if length < self.MIN_SERIAL_LENGTH:
+            fmt = "Generated sequence length must be {} or more."
+            raise ValueError(fmt.format(self.MIN_SERIAL_LENGTH))
+
+        self.timestamp = timestamp
+        self._offset = offset or 1
+        self._length = length
+        self._current = None
+        self._counter = 0
+        self._gen = None
+    
+    @property
+    def current(self):
+        if not self._gen:
+            self._gen = self.__make_iterator()
+            self._gen.__next__()
+        return self._current
+    
+    def __iter__(self):
+        if not self._gen:
+            self._gen = self.__make_iterator()
+        return self._gen
+    
+    def __make_iterator(self):
+        tstamp_str = self.timestamp.strftime('%y%m%d%H%M')
+        pad_length = self._length - self.TIMESTAMP_LENGTH
+        sn_format = '{}{:0>%s}' % pad_length
+        
+        while True:
+            self._current = sn_format.format(
+                tstamp_str, self._offset + self._counter)
+            yield self._current
+            self._counter += 1
+
+
+class HallowIndicator(object):
+    """Represents a 'hallow' progress indicator which basically sinks all
+    initiated method calls.
+    """
+    
+    def init(self, task_count=0, level=0):
+        pass
+    
+    def update(self, done=False, task_passed=None):
+        pass
+
+
+class EPXEngine(object):
+
+    EPIN_LINE_FORMAT = "{number},{serial},{value},00000,{today}"
+
+    def __init__(self, target_ext='.xml'):
+        self._target_ext = (target_ext or '.xml')
+        self.__sngen = None
+    
+    @property
+    def sngen(self):
+        if not self.__sngen:
+            self.__sngen = SNGen()
+        return self.__sngen
+    
+    @sngen.setter
+    def set_sngen(self, value):
+        self.__sngen = value
+
+    def parse(self, smsfile):
+        if isinstance(smsfile, str):
+            if not os.path.exists(smsfile):
+                raise FileNotFoundError(smsfile)
+        
+        root = ET.parse(smsfile).getroot()
+        for node in root.findall('./sms'):
+            yield Packt.parse(node.attrib['body'])
+    
+    def process(self, dirpath, indicator=None):
+        if not os.path.exists(dirpath):
+            raise ValueError("Provided directory path doesn't exist.")
+
+        result = _(errors=[], failed=[], passed=[], lines=[])
+        pInd = (indicator or HallowIndicator())
+        try:
+            filenames = self._listdir(dirpath)
+            pInd.init(task_count=len(filenames), level=0)
+        except Exception as ex:
+            result.errors.append(_(filename=None, smsno=None, error=str(ex)))
+        else:
+            for f in filenames:
+                passed = self._process_file(dirpath, f, result)
+                pInd.update(done=False, task_passed=passed)
+                if len(result.lines) >= 1000:
+                    self._flush_result(dirpath, result)
+            self._flush_result(dirpath, result)
+            pInd.update(done=True)
+        return result
+    
+    def _format_epin(self, epin):
+        return self.EPIN_LINE_FORMAT\
+                   .replace('{number}', epin.number)\
+                   .replace('{serial}', self.sngen.current)\
+                   .replace('{value}', '{:0>7}00'.format(epin.value))\
+                   .replace('{today}', self.sngen.timestamp.strftime('%d/%m/%Y'))
+    
+    def _flush_result(self, dirpath, result):
+        if result.lines:
+            fullpath = os.path.join(dirpath, 'epins.txt')
+            with open(fullpath, 'a') as f:
+                f.write('\r'.join(result.lines))
+                f.flush()
+            result.lines = []
+        
+        for label in ["passed", "failed"]:
+            if result[label]:
+                dirdest = os.path.join(dirpath, "_%s" % label)
+                self._move_files(result[label], dirpath, dirdest, result)
+
+    def _process_file(self, dirpath, filename, result):
+        passed = False
+        try:
+            lines, smsno = ([], 1)
+            for packt in self.parse(os.path.join(dirpath, filename)):
+                for epin in packt:
+                    lines.append(self._format_epin(epin))
+                smsno += 1
+            
+            result.passed.append(filename)
+            result.lines.extend(lines)
+            passed = True
+        except Exception as ex:
+            result.errors.append(_(filename=filename, smsno=smsno, error=str(ex)))
+            result.failed.append(filename)
+            passed = False
+        return passed
+    
+    def _listdir(self, dirpath):
+        if not dirpath or not os.path.isdir(dirpath):
+            raise ValueError('Invalid directory path provided.')
+        
+        filenames = []
+        for f in os.listdir(dirpath):
+            if target_ext:
+                if not f.endswith(self._target_ext):
+                    continue
+            filenames.append(f)
+        return filenames
+    
+    def _move_files(self, files, dirpath, dirdest, result):
+        if not os.path.exists(dirdest):
+            try:
+                os.mkdir(dirdest)
+            except Exception as ex:
+                err_msg = "Unabled to create directory. (Error: %s)"
+                result.errors.flush.append(err_msg % str(ex))
+        
+        if files and os.path.exists(dirdest):
+            for f in files:
+                try:
+                    shutil.move(os.path.join(dirpath, f), dirdest)
+                except Exception as ex:
+                    err_msg = "Unable to move file. (Error: %s)"
+                    result.errors.flush.append(err_msg % str(ex))
+        
--- epx/test_epx.py
+++ epx/test_epx.py
@@ -1,6 +1,11 @@
 import pytest
-from epx.core import EPin, Packt
+import os.path
+from datetime import datetime
+from epx.core import EPin, Packt, SNGen, EPXEngine
 
+
+
+FIXTURE_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
 
 
 @pytest.fixture
@@ -15,6 +20,12 @@ def packt():
         "6220427192339577,6738008238122646, Value:100 Qty:5 To recharge Dial"
         " *126*PIN# for voice or *143*PIN# for data &amp; press OK")
     return Packt.parse(message)
+
+
+@pytest.fixture
+def smsfile():
+    filename = os.path.join(FIXTURE_DIR, 'sample-smsbackup.xml')
+    return open(filename, 'r')
 
 
 class TestEPin(object):
@@ -68,3 +79,90 @@ class TestPackt(object):
             assert isinstance(epin, EPin)
             count += 1
         assert packt.count == count
+
+
+class TestSNGen(object):
+    sngen = SNGen()
+
+    def test_default_serial_startswith_default_timestamp_of_today(self):
+        serial = self.sngen.current
+        assert serial.startswith(datetime.now().strftime('%y%m%d%H'))
+    
+    def test_default_first_serial_endswith_default_offset(self):
+        serial = self.sngen.current
+        assert serial.endswith('1')
+    
+    def test_default_serial_has_length_of_20(self):
+        serial = self.sngen.current
+        assert len(serial) == 20
+    
+    def test_custom_serial_startswith_specified_timestamp(self):
+        custom_datetime = datetime(2010, 1, 1, 13, 34)
+        serial = SNGen(custom_datetime).current
+        assert serial.startswith(custom_datetime.strftime('%y%m%d%H'))
+    
+    def test_custom_first_serial_endswith_specified_offset(self):
+        offset = 2345
+        serial = SNGen(offset=offset).current
+        assert serial.endswith(str(offset))
+    
+    def test_custom_serial_length_match_specified_length(self):
+        serial_length = 17
+        serial = SNGen(length=serial_length).current
+        assert len(serial) == serial_length
+    
+    def test_creation_fails_for_length_less_than_MIN_SERIAL_LENGTH(self):
+        with pytest.raises(ValueError):
+            SNGen(length=SNGen.MIN_SERIAL_LENGTH - 1)
+
+
+class TestEXPEngine(object):
+    engine = EPXEngine()
+
+    def test_nneds_existing_dirpath_to_process(self):
+        with pytest.raises(ValueError):
+            result = self.engine.process(r'c:\fake-dir')
+    
+    def test_parse_returns_an_iterator(self, smsfile):
+        iterobj = self.engine.parse(smsfile)
+        assert hasattr(iterobj, '__iter__') == True
+    
+    def test_parse_iterator_item_is_packt_object(self, smsfile):
+        iterobj = self.engine.parse(smsfile)
+        packt = iterobj.__next__()
+        assert isinstance(packt, Packt) == True
+        assert packt.quantity == packt.count
+        assert packt.quantity == 5
+    
+    def test_epin_format_output_has_5fields_delimited_by_comma(self, epin):
+        line = self.engine._format_epin(epin)
+        assert line and ',' in line
+        assert len(line.split(',')) == 5
+    
+    def test_epin_format_output_has_pin_number_field(self, epin):
+        line = self.engine._format_epin(epin)
+        assert len(epin.number) == 16 and line.startswith(epin.number)
+    
+    def test_epin_format_output_has_serial_field(self, epin):
+        line = self.engine._format_epin(epin)
+        serial = self.engine.sngen.current
+        assert len(serial) == 20 and serial in line
+        assert line.split(',')[1] == serial
+    
+    def test_epin_format_output_has_pin_value_field(self, epin):
+        line = self.engine._format_epin(epin)
+        assert str(epin.value) in line
+        
+        value_field = line.split(',')[2]
+        assert len(value_field) == 9 and str(epin.value) in value_field
+        assert value_field == '{:0>7}00'.format(epin.value)
+    
+    def test_epin_format_output_has_dummy_five_zeros_field(self, epin):
+        five_zeros, line = '00000', self.engine._format_epin(epin)
+        assert five_zeros in line
+        assert line.split(',')[3] == five_zeros
+
+    def test_epin_format_output_has_date_field(self, epin):
+        today = self.engine.sngen.timestamp.strftime('%d/%m/%Y')
+        line = self.engine._format_epin(epin)
+        assert line.endswith(today)
